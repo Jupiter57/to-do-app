395dcdf8a76f6f8fb518691858e097a287556b32
Jupiter57 <nahashon21@gmail.comgit config --global user.name Jupiter57git config --global user.email nahashon21@gmail.com>
2018-01-09 06:43:12 +0300

    Assignment 7 completed

--- app.js
+++ app.js
@@ -18,17 +18,36 @@ function onReady(){
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    // create a new input
+    let deleteButton = document.createElement('input');
+
+    // set the input's type to button
+    deleteButton.type = "button";
+
+    //add the input's value
+    deleteButton.value = "Delete To-Do";
+
     // set the title
     newLi.textContent = title;
 
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    //attach the button to the li
+    newLi.appendChild(deleteButton);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     //empty the input
     newToDoText.value = '';
+
+    //delete list item on click of button
+    deleteButton.addEventListener('click',event =>{
+      newLi.remove();
+      //newLi.textContent = '';
+      //newLi.innerHTML = '';
+    })
   });
 }
 
